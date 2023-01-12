81eae85b080eb30b7cbeae6e859421c5af17f8f0
devthalal <thalal.hassan@neoito.com>
2023-01-12 14:15:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const divide_two_numbers = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello divide_two_numbers`}))
+  res.end()
+  
+}
+
+export default divide_two_numbers
